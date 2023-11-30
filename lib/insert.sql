8fda893aaedb240aee9220b2c2315ca450f9334f
Rowland <rowland.wanderi@student.moringaschool.com>
2023-11-30 17:28:56 +0000

    complete assignment

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 12, 'F', 'Brown','Playful', 1),
+('Rowdy', 9, 'M', 'Green','Sweet', 0),
+('Tabitha', 10, 'F', 'Blue', 'Kind', 1),
+('Sergeant Brown', 8, 'F', 'White', 'Calm', 1),
+('Melissa', 7, 'M', 'Grey', 'Territorial', 0),
+('Grinch', 6, 'M', 'Black', 'Wild', 1),
+('Wendy', 5, 'F', 'Pink', 'Reserved', 1),
+(NULL, 13, 'F', 'Orange', 'Social', 0);
